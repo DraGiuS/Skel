1bf7aa93ae9eab99b23af0a5bb4064c8b11116bc
Kristobal <christophe.ribots@gmail.com>
2018-09-29 18:51:30 +0200

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -1,2 +1,5 @@
 plasmapkg2 -i uswitch-v1-2-1.plasmoid
 plasmapkg2 -i umenu-v1.4.plasmoid
+git clone https://github.com/Polunom/mac-inline-battery
+cd mac-inline-battery 
+kpackagetool5 -t Plasma/Applet --install .
