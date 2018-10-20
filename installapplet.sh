32a4fbbeed921325d773fc96e4effda6e4ebc548
Kristobal <christophe.ribots@gmail.com>
2018-10-20 18:55:50 +0200

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -1,5 +1,6 @@
 plasmapkg2 -i uswitch-v1-2-1.plasmoid
 plasmapkg2 -i umenu-v1.4.plasmoid
+kpackagetool5 -t Plasma/Wallpaper -i VideoWallpaper.tar.gz
 git clone https://github.com/Polunom/mac-inline-battery
 cd mac-inline-battery 
 kpackagetool5 -t Plasma/Applet --install .
