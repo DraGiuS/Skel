ff8bba6a95b8f0becb5cd0923fb793f00c346300
Kristobal <christophe.ribots@gmail.com>
2018-10-28 17:57:56 +0100

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -2,3 +2,6 @@ plasmapkg2 -i uswitch-v1-2-1.plasmoid
 plasmapkg2 -i umenu-v1.4.plasmoid
 kpackagetool5 -t Plasma/Wallpaper -i VideoWallpaper.tar.gz
 kpackagetool5 -t Plasma/Applet -i org.kde.plasma.inlineBattery.tar.gz
+git clone https://github.com/Zren/plasma-applet-volumewin7mixer.git
+cd plasma-applet-volumewin7mixer
+./install
