da9a39bb280ca1dcace98f94d6028f1ed12876ec
Kristobal <christophe.ribots@gmail.com>
2018-11-03 17:18:45 +0100

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -2,5 +2,5 @@ plasmapkg2 -i uswitch-v1-2-1.plasmoid
 plasmapkg2 -i umenu-v1.4.plasmoid
 kpackagetool5 -t Plasma/Wallpaper -i VideoWallpaper.tar.gz
 kpackagetool5 -t Plasma/Applet -i org.kde.plasma.inlineBattery.tar.gz
-cd plasma-applet-volumewin7mixer
-bash ./install
+bash ./vscode_setup.sh
+
