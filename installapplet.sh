db159751784952e41fca60f6c9ab40b6bf2928c9
Kristobal <christophe.ribots@gmail.com>
2018-11-10 22:20:50 +0100

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -1,6 +1,9 @@
 plasmapkg2 -i uswitch-v1-2-1.plasmoid
 plasmapkg2 -i umenu-v1.4.plasmoid
 kpackagetool5 -t Plasma/Wallpaper -i VideoWallpaper.tar.gz
-kpackagetool5 -t Plasma/Applet -i org.kde.plasma.inlineBattery.tar.gz
+git clone https://github.com/ishovkun/mac-inline-battery
+cd mac-inline-battery
+kpackagetool5 -t Plasma/Applet --install .
+cd ..
 bash ./vscode_setup.sh
 
