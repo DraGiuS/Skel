f35e9cb5a826ed25a587677004d5bafbc484f274
Yosh <christophe.ribots@gmail.com>
2018-11-18 13:35:42 +0100

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -1,9 +1,15 @@
 plasmapkg2 -i uswitch-v1-2-1.plasmoid
-plasmapkg2 -i umenu-v1.4.plasmoid
 kpackagetool5 -t Plasma/Wallpaper -i VideoWallpaper.tar.gz
 sudo git clone https://github.com/ishovkun/mac-inline-battery
 cd mac-inline-battery
 kpackagetool5 -t Plasma/Applet --install .
+cd /home/"$USER"/
+sudo git clone https://gitlab.com/jnuutinen/minimal-menu
+cd minimal-menu
+mkdir build
+cd build
+cmake ..
+make install
 cd /home/"$USER"/
 git clone https://github.com/maui-project/index --recursive #Install index instead of dolphin
 cd index
