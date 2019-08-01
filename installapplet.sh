79b2cd47408efacb549608f96a142e9536ea5b8d
Yosh <christophe.ribots@gmail.com>
2019-08-02 00:23:52 +0200

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -2,32 +2,35 @@ plasmapkg2 -i uswitch-v1-2-1.plasmoid
 sudo git clone https://github.com/ishovkun/mac-inline-battery
 cd mac-inline-battery
 kpackagetool5 -t Plasma/Applet --install .
-cd /home/"$USER"/
+cd /root/arch_installer/
 sudo git clone https://github.com/wsdfhjxc/virtual-desktop-bar
-sudo mkdir build
+cd virtual-desktop-bar/
+mkdir build
 cd build
 sudo cmake ..
-sudo make 
+sudo make
 sudo make install
-cd /home/"$USER"/
+cd /root/arch_installer/
 sudo git clone https://github.com/psifidotos/latte-indicator-dashtopanel/
 cd latte-indicator-dashtopanel
 kpackagetool5 -i . -t Latte/Indicator
-cd /home/"$USER"/
+cd /root/arch_installer/
 sudo git clone https://github.com/yagomont/material-decoration-bt.git
 cd material-decoration-bt; bash setup.sh
-cd /home/"$USER"/
+cd /root/arch_installer/
 sudo git clone https://github.com/zren/plasma-applet-tiledmenu
 cd plasma-applet-tiledmenu
 kpackagetool5 -t Plasma/Applet -i package
-cd /home/"$USER"/
+cd /root/arch_installer/
 sudo git clone https://github.com/jsalatas/plasma-pstate
 cd plasma-pstate
 sudo ./install.sh
-cd /home/"$USER"/
+cd /root/arch_installer/
 git clone https://github.com/psifidotos/applet-window-title/
 cd applet-window-title
 plasmapkg2 -i .
-cd /home/"$USER"/skel/
+cd /root/arch_installer/Skel/
 bash ./vscode_setup.sh # Install vscode configs
+
+
 
