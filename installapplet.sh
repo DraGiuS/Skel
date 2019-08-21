d535375eee8ef3a596c8848d8520aaf1744a6a7e
Yosh <christophe.ribots@gmail.com>
2019-08-21 17:16:42 +0200

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -1,4 +1,6 @@
 plasmapkg2 -i uswitch-v1-2-2.plasmoid
+cd /root/arch_installer/Skel/kwin-quarter-tiling
+bash ./install.sh
 sudo git clone https://github.com/ishovkun/mac-inline-battery
 cd mac-inline-battery
 kpackagetool5 -t Plasma/Applet --install .
