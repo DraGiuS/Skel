1a6fab3f6f80de0efd760cf449a9f9625965541c
Yosh <christophe.ribots@gmail.com>
2019-08-23 16:52:44 +0200

    Added spicetify configs

--- installapplet.sh
+++ installapplet.sh
@@ -1,3 +1,5 @@
+# Kde applets
+
 plasmapkg2 -i uswitch-v1-2-2.plasmoid
 cd /root/arch_installer/Skel/kwin-quarter-tiling
 bash ./install.sh
@@ -17,14 +19,6 @@ sudo git clone https://github.com/psifidotos/latte-indicator-dashtopanel/
 cd latte-indicator-dashtopanel
 kpackagetool5 -i . -t Latte/Indicator
 cd /root/arch_installer/
-sudo git clone https://github.com/marioortizmanero/spotify-music-videos
-cd spotify-music-videos
-sudo python ./setup.py install
-python ./setup.py install --user
-cd /root/arch_installer/
-sudo git clone https://github.com/yagomont/material-decoration-bt.git
-cd material-decoration-bt; bash setup.sh
-cd /root/arch_installer/
 sudo git clone https://github.com/zren/plasma-applet-tiledmenu
 cd plasma-applet-tiledmenu
 kpackagetool5 -t Plasma/Applet -i package
@@ -36,8 +30,34 @@ cd /root/arch_installer/
 git clone https://github.com/psifidotos/applet-window-title/
 cd applet-window-title
 plasmapkg2 -i .
+
+# Spotify-music-videos
+
+cd /root/arch_installer/
+sudo git clone https://github.com/marioortizmanero/spotify-music-videos
+cd spotify-music-videos
+sudo python ./setup.py install
+python ./setup.py install --user
+# Window style
+
+cd /root/arch_installer/
+sudo git clone https://github.com/yagomont/material-decoration-bt.git
+cd material-decoration-bt; bash setup.sh
+
+# Spicetify 
+sudo chmod 777 /opt/spotify -R
+spicetify
+spicetify backup apply enable-devtool
+spicetify update
+cd "$(dirname "$(spicetify -c)")/Themes"
+git clone https://github.com/khanhas/google-spicetify
+spicetify config current_theme google-spicetify
+spicetify config current_theme google-spicetify; spicetify config color_scheme gow; spicetify apply
+
+# Install vscode configs
+
 cd /root/arch_installer/Skel/
-bash ./vscode_setup.sh # Install vscode configs
+bash ./vscode_setup.sh 
 
 
 
