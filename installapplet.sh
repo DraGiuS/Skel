d3e852880da5d8077d85313c666ea280da5dbe9c
Yosh <christophe.ribots@gmail.com>
2020-03-22 11:26:19 +0100

    Update installapplet.sh

--- installapplet.sh
+++ installapplet.sh
@@ -6,14 +6,6 @@ sudo git clone https://github.com/ishovkun/mac-inline-battery
 cd mac-inline-battery
 kpackagetool5 -t Plasma/Applet --install .
 cd /root/arch_installer/
-sudo git clone https://github.com/wsdfhjxc/virtual-desktop-bar
-cd virtual-desktop-bar/
-mkdir build
-cd build
-sudo cmake ..
-sudo make
-sudo make install
-cd /root/arch_installer/
 sudo git clone https://github.com/psifidotos/latte-indicator-dashtopanel/
 cd latte-indicator-dashtopanel
 kpackagetool5 -i . -t Latte/Indicator
