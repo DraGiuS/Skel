6df4dfb1c6bd7ab7b74bf29af4daa1f265f938ce
Yosh <christophe.ribots@gmail.com>
2020-03-23 01:31:06 +0100

    Update vscode_setup.sh

--- vscode_setup.sh
+++ vscode_setup.sh
@@ -9,7 +9,7 @@ extension_general="ms-vscode.cpptools  kevinkyang.auto-comment-blocks CoenraadS.
 msjsdiag.debugger-for-chrome eamodio.gitlens donjayamanne.githistory huizhou.githd webfreak.debug  \
 wayou.vscode-todo-highlight emilast.logfilehighlighter Tyriar.sort-lines Gimly81.matlab \
 timonwong.shellcheck tomoki1207.pdf oderwat.indent-rainbow rashwell.tcl \
-vector-of-bool.cmake-tools twxs.cmake eugenwiens.bitbake redhat.vscode-yaml "
+twxs.cmake eugenwiens.bitbake redhat.vscode-yaml "
 
 extension_theme=" zhuangtongfa.material-theme monokai.theme-monokai-pro-vscode "
 
